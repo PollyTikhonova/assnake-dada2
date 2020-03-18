a269205aa537fecd7805cf4657a861eac202af39
Fedorov113 <fedorov.de@gmail.com>
2020-03-19 02:07:05 +0300

    Update invocation_commands.py

--- assnake_dada2/invocation_commands.py
+++ assnake_dada2/invocation_commands.py
@@ -2,28 +2,31 @@ import assnake.api.loaders
 import assnake.api.sample_set
 from tabulate import tabulate
 import click
-from assnake.cli.cli_utils import options_w_params, add_options, generic_command_individual_samples, generate_result_list
-
+from assnake.cli.cli_utils import sample_set_construction_options, add_options, generic_command_individual_samples, generate_result_list
+import os
+import pandas as pd
 
 @click.command('dada2-filter-and-trim', short_help='Filter and trim your reads with dada2 trimmer')
-@add_options(options_w_params)
+@add_options(sample_set_construction_options)
+@click.option('--params', help='Parameters to use', default='def', type=click.STRING )
 @click.pass_obj
+
 def filter_and_trim_invocation(config, result = 'dada2-filter-and-trim', **kwargs):
+    
     wc_str = '{fs_prefix}/{df}/reads/{preproc}__dada2fat_{params}/{sample}_R1.fastq.gz'
-    sample_set = generic_command_individual_samples(config,  **kwargs)
+    sample_set, sample_set_name = generic_command_individual_samples(config,  **kwargs)
     config['requests'] += generate_result_list(sample_set, wc_str, **kwargs)
     
 
 
 @click.command('dada2-full', short_help='Execute full dada2 pipeline')
 
-@click.option('--df','-d', help='Name of the dataset', required=True )
-@click.option('--preproc','-p', help='Preprocessing to use' )
-@click.option('--samples-to-add','-s', 
-                help='Samples from dataset to process', 
-                default='', 
-                metavar='<samples_to_add>', 
+@add_options(sample_set_construction_options)
+@click.option('--sample-set-name', 
+                help='Name of your sample set', 
+                default='',
                 type=click.STRING )
+
 @click.option('--learn-errors-params', 
                 help='Parameters for learn errors', 
                 default='def',
@@ -34,32 +37,61 @@ def filter_and_trim_invocation(config, result = 'dada2-filter-and-trim', **kwarg
                 type=click.STRING )
 
 @click.pass_obj
-def dada2_full(config, df, preproc, samples_to_add, learn_errors_params, min_overlap):
+def dada2_full(config, sample_set_name, learn_errors_params, min_overlap, **kwargs):
+
     # check if database initialized
     if config['config'].get('dada2-silva_nr_v132', None) is None:
         click.secho('Silva database not initialized!', fg='red')
         click.echo('run ' + click.style('assnake init dada2-silva-db', bg='blue') + ' and follow instructions')
         exit()
-    samples_to_add = [] if samples_to_add == '' else [c.strip() for c in samples_to_add.split(',')]
-    df = assnake.api.loaders.load_df_from_db(df)
-    config['requested_dfs'] += [df['df']]
-    ss = assnake.api.sample_set.SampleSet(df['fs_prefix'], df['df'], preproc, samples_to_add=samples_to_add)
+    print(kwargs)
 
-    click.echo(tabulate(ss.samples_pd[['fs_name', 'reads', 'preproc']].sort_values('reads'), 
-        headers='keys', tablefmt='fancy_grid'))
+    # load sample set     
+    sample_set, sample_set_name = generic_command_individual_samples(config,  **kwargs)
 
-    set_name='sample_set'
-    ss.prepare_dada2_sample_list(set_name)
-    res_list = ['{fs_prefix}/{df}/dada2/{sample_set}/learn_erros__{params}/taxa_{len}.rds'.format(
-        fs_prefix = df['fs_prefix'].rstrip('\/'),
-        df = df['df'],
-        sample_set = set_name,
-        params = learn_errors_params,
-        len = min_overlap
+    # Prepare sample set file
+    res_list = prepare_sample_set_tsv_and_get_results(sample_set, sample_set_name, learn_errors_params = learn_errors_params, min_overlap = min_overlap)
+
+    config['requests'] += res_list
+
+def prepare_sample_set_tsv_and_get_results(sample_set, sample_set_name, **kwargs):
+
+    dfs = list(set(sample_set.samples_pd['df']))
+    if len(dfs) == 1:
+        fs_prefix = list(set(sample_set.samples_pd['fs_prefix']))[0]
+
+    dada2_set_dir_wc = '{fs_prefix}/{df}/dada2/{sample_set_name}'
+    dada2_set_dir = '{fs_prefix}/{df}/dada2/{sample_set_name}/'.format(fs_prefix = fs_prefix, df = dfs[0], sample_set_name = sample_set_name)
+    from string import Formatter
+    import string
+    field_names = [name for text, name, spec, conv in string.Formatter().parse(dada2_set_dir_wc)]
+
+    print(field_names)
+
+    # lst = { }
+
+    # print(dada2_set_dir_wc.format( **{key:value for key,value in kwargs.items() if key in field_names} ))
+
+    dada2_dicts = []
+    for s in sample_set.samples_pd.to_dict(orient='records'):
+        dada2_dicts.append(dict(mg_sample=s['fs_name'],
+        R1 = sample_set.wc_config['fastq_gz_file_wc'].format(fs_prefix=s['fs_prefix'], df=s['df'], preproc=s['preproc'], sample = s['fs_name'], strand = 'R1'), 
+        R2 = sample_set.wc_config['fastq_gz_file_wc'].format(fs_prefix=s['fs_prefix'], df=s['df'], preproc=s['preproc'], sample = s['fs_name'], strand = 'R2'),
+        # merged = sample_set.wc_config['dada2_merged_wc'].format(prefix=s['fs_prefix'], df=s['df'], preproc=s['preproc'], sample = s['fs_name'], sample_set = set_name)
+        ))
+    if not os.path.exists(dada2_set_dir):
+        os.makedirs(dada2_set_dir, exist_ok=True)
+
+    dada2_df = pd.DataFrame(dada2_dicts)
+    if not os.path.isfile(os.path.join(dada2_set_dir, 'samples.tsv')):
+        dada2_df.to_csv(os.path.join(dada2_set_dir, 'samples.tsv'), sep='\t', index=False)
+
+    res_list = ['{fs_prefix}/{df}/dada2/{sample_set}/learn_erros__{learn_errors_params}/taxa_{min_overlap}.rds'.format(
+        fs_prefix = fs_prefix,
+        df = dfs[0],
+        sample_set = sample_set_name,
+        learn_errors_params = kwargs['learn_errors_params'],
+        min_overlap = kwargs['min_overlap']
     )]
 
-
-    if config.get('requests', None) is None:
-        config['requests'] = res_list
-    else:
-        config['requests'] += res_list
+    return res_list
