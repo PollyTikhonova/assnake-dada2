27f09237f720eba428485d4a764ae609ec422bc3
Fedorov113 <fedorov.de@gmail.com>
2020-03-18 22:49:37 +0300

    Filter and trim done with generic wrapper

--- assnake_dada2/invocation_commands.py
+++ assnake_dada2/invocation_commands.py
@@ -2,46 +2,16 @@ import assnake.api.loaders
 import assnake.api.sample_set
 from tabulate import tabulate
 import click
+from assnake.cli.cli_utils import options_w_params, add_options, generic_command_individual_samples
 
 
 @click.command('dada2-filter-and-trim', short_help='Filter and trim your reads')
-
-@click.option('--df','-d', help='Name of the dataset', required=True )
-@click.option('--preproc','-p', help='Preprocessing to use' )
-@click.option('--samples-to-add','-s', 
-                help='Samples from dataset to process', 
-                default='', 
-                metavar='<samples_to_add>', 
-                type=click.STRING )
-@click.option('--params', 
-                help='Parameters to use', 
-                default='def',
-                type=click.STRING )
+@add_options(options_w_params)
 @click.pass_obj
-def filter_and_trim_invocation(config, df, preproc, samples_to_add, params):
-    samples_to_add = [] if samples_to_add == '' else [c.strip() for c in samples_to_add.split(',')]
-    df = assnake.api.loaders.load_df_from_db(df)
-    config['requested_dfs'] += [df['df']]
-    ss = assnake.api.sample_set.SampleSet(df['fs_prefix'], df['df'], preproc, samples_to_add=samples_to_add)
+def filter_and_trim_invocation(config, result = 'dada2-filter-and-trim', **kwargs):
+    generic_command_individual_samples(config, wc_str = '{fs_prefix}/{df}/reads/{preproc}__dada2fat_{params}/{sample}_R1.fastq.gz', **kwargs)
 
-    click.echo(tabulate(ss.samples_pd[['fs_name', 'reads', 'preproc']].sort_values('reads'), 
-        headers='keys', tablefmt='fancy_grid'))
-    res_list = []
-
-    for s in ss.samples_pd.to_dict(orient='records'):
-        preprocessing = s['preproc']
-        res_list.append( '{fs_prefix}/{df}/reads/{preproc}__dada2fat_{params}/{sample}_R1.fastq.gz'.format(
-            fs_prefix = s['fs_prefix'].rstrip('\/'),
-            df = s['df'],
-            preproc = preprocessing,
-            sample = s['fs_name'],
-            params = params
-        ))
-
-    if config.get('requests', None) is None:
-        config['requests'] = res_list
-    else:
-        config['requests'] += res_list
+    
 
 
 @click.command('dada2-full', short_help='Execute full dada2 pipeline')
