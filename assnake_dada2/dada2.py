6e49f610745f94c31fc5e562280144c8a06a1e1a
Fedorov113 <fedorov.de@gmail.com>
2020-03-27 14:41:17 +0300

    Update dada2.py
    .get silva db

--- assnake_dada2/dada2.py
+++ assnake_dada2/dada2.py
@@ -80,7 +80,7 @@ rule dada2_nochim:
 
 
 assign_taxa_script = os.path.join(config['assnake-dada2'], 'scripts/assign_taxa.R')
-db_silva_nr_v132 = config['dada2-silva_nr_v132']
+db_silva_nr_v132 = config.get('dada2-silva_nr_v132', None)
 rule dada2_assign_taxa:
     input: '{fs_prefix}/{df}/dada2/{sample_set}/learn_erros__{err_params}/seqtab_nochim__{min_overlap}.rds'
     output: '{fs_prefix}/{df}/dada2/{sample_set}/learn_erros__{err_params}/taxa_{min_overlap}.rds'
